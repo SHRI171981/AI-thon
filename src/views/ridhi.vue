2514bc38d822cea9dca686d32ae3a0c2aa84c65f
ridhi <sharmaridhi765@gmail.com>
2025-03-26 16:50:04 +0530

    adding changes to ridhi.vue

---
+++ src/views/ridhi.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="number in numbers" :key="number">{{ number }}</li>
+    </ul>
+    <ReadNews />
+  </div>
+</template>
+
+<script>
+import ReadNews from '@/components/ReadNews.vue';
+
+export default {
+  components: {
+    ReadNews
+  },
+  data() {
+    return {
+      numbers: [1, 2, 3, 4, 5]
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* Add any styles you need here */
+</style>
